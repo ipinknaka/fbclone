276423dac4e6b96588472b6baf168d05516c317c
ipinknaka <ipinknaka@bingzone.net>
2022-04-28 02:12:24 +0000

    login

--- components/Login.js
+++ components/Login.js
@@ -1,11 +1,12 @@
 import Image from 'next/image'
 import logo from '../public/GitHub-Mark-120px-plus.png'
+import { signIn } from "next-auth/react"
 
 function Login() {
   return (
-    <div className='grid place-items-center'>
+    <div className='grid place-items-center mt-5 background-pattern'>
       <Image src={logo} width={120} height={120} objectFit='contain' />
-      <h1 className='mt-5 p-5 rounded-full bg-black text-white text-center cursor-pointer'>Login with Github</h1>
+      <h1 onClick={signIn} className='mt-5 p-5 rounded-full bg-black text-white text-center cursor-pointer'>Login with Github</h1>
     </div>
   )
 }
